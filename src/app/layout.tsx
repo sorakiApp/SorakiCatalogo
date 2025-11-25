2cc62c781c99d3af0728565f079cb170c66ba829
Soraki <sorakiapp@gmail.com>
2025-11-25 17:25:25 +0000

    iiiiiiii

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap" rel="stylesheet" />
-        <link rel="icon" href="/soraki.png" type="image/png"></link>
       </head>
       <body className="font-body antialiased">{children}<Toaster /></body>
     </html>
